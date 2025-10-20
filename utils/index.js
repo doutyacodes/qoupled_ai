73de64e8f130b1cdaf87a85b615209e3fab358dd
shabeersf <shabeermen@gmail.com>
2025-10-20 14:19:48 +0530

    updated link

--- utils/index.js
+++ utils/index.js
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise";
 
 // Use environment variables for database connection
 const connection = await mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  database: process.env.DB_NAME || "devuser_qoupled_upgrade",
-  password: process.env.DB_PASSWORD || '',
-  port: process.env.DB_PORT || '3306'
+  host: "68.178.163.247",
+  user: "devuser_qoupled_upgrade",
+  database: "devuser_qoupled_upgrade",
+  password:'Wowfy#user',
+  port:'3306'
 });
-
 export const db = drizzle(connection);
+
